868337bd300d8f7db38ec530cbba9fd96e0ef0b6
Hiago75 <hiagobrenha@protonmail.com>
2021-07-13 19:03:04 -0300

    Create categories on database, categories entity

---
+++ src/entities/Categories.ts
@@ -0,0 +1,23 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn } from 'typeorm';
+import { v4 as uuid } from 'uuid';
+
+@Entity('categories')
+export class Categories {
+  @PrimaryColumn()
+  readonly id: string;
+
+  @Column()
+  name: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
+}
