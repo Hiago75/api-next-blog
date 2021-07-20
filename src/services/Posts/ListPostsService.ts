5fe4424d51f99e783534feae72959ecac9720ca1
Hiago75 <hiagobrenha@protonmail.com>
2021-07-20 20:23:39 -0300

    Remove unnecessary if statement

--- src/services/Posts/ListPostsService.ts
+++ src/services/Posts/ListPostsService.ts
@@ -4,13 +4,9 @@ import { PostsRepositories } from '../../repositories/PostsRepositories';
 export class ListPostsService {
   async execute(start: number, limit: number) {
     const postsRepositories = getCustomRepository(PostsRepositories);
-    let posts;
 
-    if (start || limit) {
-      posts = postsRepositories.findAndPaginate(start, limit);
-    } else {
-      posts = postsRepositories.findAndOrder();
-    }
+    const posts = postsRepositories.findAndPaginate(start, limit);
+
     return posts;
   }
 }
