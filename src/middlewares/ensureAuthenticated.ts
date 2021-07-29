c544245bccbf024e9393886c1cbae1622557b9c2
Hiago75 <hiagobrenha@protonmail.com>
2021-07-29 15:36:51 -0300

    Update ensureAuthenticated middleware verifications

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -1,6 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
 import { verify } from 'jsonwebtoken';
-import { Unauthorized } from '../custom/errors/Unauthorized';
 
 interface IPayload {
   sub: string;
@@ -8,20 +7,27 @@ interface IPayload {
 
 // Ensure that user has logged on application before doing something
 export function ensureAuthenticated(request: Request, response: Response, next: NextFunction) {
-  // Try to get token from headers and
   const authToken = request.headers.authorization;
-  if (!authToken) throw new Unauthorized('You need to login to access this page');
 
-  // Divide the "Bearer" word from the token itself
-  const [, token] = authToken.split(' ');
+  if (!authToken) return response.status(401).send({ error: 'You need to login to access this page' });
+
+  // Divide the "Bearer" word from the token itself and test if token have these 2 parts
+  const parts = authToken.split(' ');
+  if (parts.length !== 2) return response.status(401).send({ error: 'Token error' });
+
+  // Test if the scheme is "Bearer"
+  const [scheme, token] = parts;
+  if (!/^Bearer$/i.test(scheme)) return response.status(401).send({ error: 'Invalid token format' });
 
   // Verify if token is valid and inject on request
-  try {
-    const { sub } = verify(token, process.env.TOKEN_SECRET as string) as IPayload;
+  verify(token, process.env.TOKEN_SECRET as string, (error, decoded) => {
+    if (error) return response.status(401).send({ error: 'Token is invalid' });
+    if (!decoded) throw new Error('Unexpected error');
+
+    const { sub } = decoded as IPayload;
+
     request.user_id = sub;
 
     return next();
-  } catch (e) {
-    throw new Unauthorized('Invalid token');
-  }
+  });
 }
