68db7a8c36a3ead39d56e4e003a1aba76fea5d08
Hiago75 <hiagobrenha75@gmail.com>
2021-08-11 15:53:23 -0300

    chore: update providers responses adding pt-BR traslations

--- src/provider/CloudinaryUpload.ts
+++ src/provider/CloudinaryUpload.ts
@@ -4,7 +4,7 @@ import { BadRequest } from '../custom/errors';
 export async function uploadToCloudinary(file: string, formats?: { width: number }[]) {
   const uploadedPhoto = await cloudinary.uploader.upload(file, { eager: formats || '' });
 
-  if (!uploadedPhoto) throw new BadRequest('Photo can`t be uploaded');
+  if (!uploadedPhoto) throw new BadRequest('photo_upload_error');
 
   const { original_filename: name, public_id: publicId, width, height, secure_url: url, eager } = uploadedPhoto;
 
