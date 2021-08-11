ae2e488fa8b140946086c2c9451f692399e6a34f
Hiago75 <hiagobrenha75@gmail.com>
2021-08-11 15:55:29 -0300

    chore: update utils responses adding pt-BR traslations

--- src/utils/formatToken.ts
+++ src/utils/formatToken.ts
@@ -2,10 +2,10 @@ import { Unauthorized } from '../custom/errors';
 
 export function formatToken(rawToken: string) {
   const parts = rawToken.split(' ');
-  if (parts.length !== 2) throw new Unauthorized('Token error');
+  if (parts.length !== 2) throw new Unauthorized('invalid_token_format');
 
   const [scheme, token] = parts;
-  if (!/^Bearer$/i.test(scheme)) throw new Unauthorized('Invalid token format');
+  if (!/^Bearer$/i.test(scheme)) throw new Unauthorized('invalid_token_format');
 
   return token;
 }
