7791b4458fce3fc7d00b932351fa159d47e4bc48
Hiago75 <hiagobrenha75@gmail.com>
2021-08-14 12:42:30 -0300

    test: add list categories implementation test

---
+++ __tests__/integration/listCategories.spec.ts
@@ -0,0 +1,17 @@
+import app from '../../src/app';
+import request from 'supertest';
+
+import { testFactory } from '../utils/testFactory';
+import { categoryFactory } from '../utils/factories/categoryFactory';
+
+describe('List categories', () => {
+  testFactory();
+
+  it('should list all the categories', async () => {
+    await categoryFactory('Test category');
+    await categoryFactory('Another test category');
+    const response = await request(app).get(`/categories`);
+
+    expect(response.body.length).toEqual(2);
+  });
+});
