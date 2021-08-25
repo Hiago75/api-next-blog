47a1abe00b295ddbdf80f63527d585aefaae9f16
Hiago75 <hiagobrenha75@gmail.com>
2021-08-25 15:58:06 -0300

    config: Add heroku config

--- ormconfig.js
+++ ormconfig.js
@@ -1,5 +1,15 @@
 module.exports = [
   {
+    type: 'postgres',
+    url: process.env.DATABASE_URL,
+    migrations: ['dist/database/migrations/*.js'],
+    entities: ['dist/entities/*.js'],
+    cli: {
+      migrationsDir: 'src/database/migrations',
+      entitiesDir: 'src/entities',
+    },
+  },
+  {
     name: 'production',
     type: 'postgres',
     url: process.env.DATABASE_URL,
