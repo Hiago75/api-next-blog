c06ba5e9d6a075b60894fcfbdc9ba859924befcc
Hiago75 <hiagobrenha75@gmail.com>
2021-12-05 14:18:55 -0300

    feat: update post support to external photo url

--- src/controllers/Posts/UpdatePostsController.ts
+++ src/controllers/Posts/UpdatePostsController.ts
@@ -4,7 +4,7 @@ import { BadRequest } from '../../custom/errors';
 import { UpdatePostsService } from '../../services';
 
 export class UpdatePostsController {
-  constructor(private updatePostsService: UpdatePostsService) {}
+  constructor(private updatePostsService: UpdatePostsService) { }
 
   async handle(request: Request, response: Response) {
     const { id } = request.params;
@@ -18,11 +18,15 @@ export class UpdatePostsController {
       }
     }
 
-    const updatedPost = await this.updatePostsService.execute({ postId: id, itemsToBeUpdated });
+    try {
+      const updatedPost = await this.updatePostsService.execute({ postId: id, itemsToBeUpdated });
 
-    if (process.env.NODE_ENV === 'production')
-      axios.post('https://api.netlify.com/build_hooks/61675fa4f2f8f62777bca770');
+      if (process.env.NODE_ENV === 'production')
+        axios.post('https://api.netlify.com/build_hooks/61675fa4f2f8f62777bca770');
 
-    return response.json(updatedPost);
+      return response.json(updatedPost);
+    } catch (e) {
+      console.log(e);
+    }
   }
 }
